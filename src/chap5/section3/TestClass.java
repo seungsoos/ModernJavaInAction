8b3c9451c3b4994b590d00b843888a6fedd54f0b
seungsoo <seung_soos@naver.com>
2023-07-17 22:13:13 +0900

    chap5  - section3

--- src/chap5/section3/TestClass.java
+++ src/chap5/section3/TestClass.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestClass {
 
@@ -36,5 +37,57 @@ public class TestClass {
                 .distinct().collect(Collectors.toList());
 
         result.stream().forEach(x -> System.out.println(Arrays.toString(x)));
+
+        /**
+         * map과 Arrays.stream 활용
+         */
+        List<String> words = null;
+        String[] arraysOfWord = {"Goodbye", "World"};
+        Stream<String> streamOfWords = Arrays.stream(arraysOfWord);
+        words.stream()
+                .map(word -> word.split("")) // 각 단어를 개별 문자열로 반환
+                .map(Arrays::stream) // 각 배열을 별도의 스트림으로 생성
+                .distinct()
+                .collect(Collectors.toList());
+
+        /**
+         * flatMap 사용
+         */
+        List<String> uniqueCharacters = words.stream()
+                .map(word -> word.split("")) // 각 단어를 개별 문자를 포함하는 배열로 변환
+                .flatMap(Arrays::stream) // 생성된 스트림을 하나의 스트림으로 평면화
+                .distinct()
+                .collect(Collectors.toList());
+
+        /**
+         * Quiz 1.
+         */
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> collect = numbers.stream()
+                .map(x -> x * x)
+                .collect(Collectors.toList());
+        System.out.println("collect = " + collect);
+
+        /**
+         * Quiz 2.
+         */
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(3, 4);
+
+        List<int[]> collect1 = numbers1.stream()
+                .flatMap(i -> numbers2.stream()
+                        .map(j -> new int[]{i, j}))
+                .collect(Collectors.toList());
+
+        /**
+         * Quiz 3.
+         */
+        List<int[]> collect2 = numbers1.stream()
+                .flatMap(i -> numbers2.stream()
+                        .filter(j -> (i+j) % 3 == 0)
+                        .map(j -> new int[]{i, j}))
+                .collect(Collectors.toList());
+
     }
+
 }
